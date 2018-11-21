c5dcb81dc53d14231a09174e01ef07f77bd10ae1
Daehee Kim <daehee.kim@theloop.co.kr>
2018-11-21 11:40:39 +0900

    Merge exceptions.py to exception.py

--- loopchain/blockchain/exception.py
+++ loopchain/blockchain/exception.py
@@ -35,6 +35,22 @@ class BlockchainError(Exception):
     pass
 
 
+class BlockVersionNotMatch(Exception):
+    def __init__(self, block_version: str, target_version: str, msg: str):
+        super().__init__(msg)
+        self.block_version = block_version
+        self.target_version = target_version
+        self.msg = msg
+
+    def __str__(self):
+        results = []
+        if self.msg:
+            results.append(self.msg)
+        results.append(f"block version: {self.block_version}")
+        results.append(f"target version: {self.target_version}")
+        return ' '.join(results)
+
+
 class ScoreInvokeError(Exception):
     """Error While Invoke Score
     """
