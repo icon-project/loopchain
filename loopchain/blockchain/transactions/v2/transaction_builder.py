4adb11a26337df1e6a23c33b15ca7b6f244a1760
Daehee Kim <daehee.kim@theloop.co.kr>
2018-12-03 16:27:03 +0900

    Fix the problem that v2 tx nonce was handled by hex string

--- loopchain/blockchain/transactions/v2/transaction_builder.py
+++ loopchain/blockchain/transactions/v2/transaction_builder.py
@@ -63,6 +63,6 @@ class TransactionBuilder(BaseTransactionBuilder):
             "timestamp": str(self._timestamp)
         }
         if self.nonce is not None:
-            params["nonce"] = hex(self.nonce)
+            params["nonce"] = str(self.nonce)
 
         return Hash32(self._hash_generator.generate_hash(params))
--- loopchain/blockchain/transactions/v2/transaction_serializer.py
+++ loopchain/blockchain/transactions/v2/transaction_serializer.py
@@ -16,7 +16,7 @@ class TransactionSerializer(BaseTransactionSerializer):
         if tx.timestamp is not None:
             params['timestamp'] = str(tx.timestamp)
         if tx.nonce is not None:
-            params['nonce'] = hex(tx.nonce)
+            params['nonce'] = str(tx.nonce)
         return params
 
     def to_raw_data(self, tx: 'Transaction'):
@@ -40,7 +40,7 @@ class TransactionSerializer(BaseTransactionSerializer):
             to_address=Address.fromhex(tx_data['to']),
             value=int(tx_data['value'], 16),
             fee=int(tx_data['fee'], 16),
-            nonce=int(nonce, 16) if nonce is not None else None
+            nonce=int(nonce) if nonce is not None else None
         )
 
     def get_hash(self, tx_dumped: dict) -> str:
