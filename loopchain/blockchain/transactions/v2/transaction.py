d5c61e839b397a4d1b47fd329f2c6ed7e3a92fae
Daehee Kim <daehee.kim@theloop.co.kr>
2018-12-13 11:02:42 +0900

    [LC-79] Add `MalformedStr` type hint to nonce parameter in v2 tx.

--- loopchain/blockchain/transactions/v2/transaction.py
+++ loopchain/blockchain/transactions/v2/transaction.py
@@ -21,7 +21,8 @@ class Transaction(BaseTransition):
 
     def __init__(self, hash: 'Hash32', signature: 'Signature', timestamp: int,
                  from_address: 'Address', to_address: 'Address',
-                 value: Union[int, MalformedStr], fee: Union[int, MalformedStr], nonce: int, extra: Mapping[str, str]):
+                 value: Union[int, MalformedStr], fee: Union[int, MalformedStr], nonce: Union[int, MalformedStr],
+                 extra: Mapping[str, str]):
         super().__init__(hash, signature, timestamp)
 
         object.__setattr__(self, "from_address", from_address)
