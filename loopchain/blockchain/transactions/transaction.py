ac1dfbf8e16656854d8cdb2a9444300005abf766
Daehee Kim <daehee.kim@theloop.co.kr>
2018-12-13 10:58:46 +0900

    [LC-79] Change a method of calculating tx size.

--- loopchain/blockchain/transactions/transaction.py
+++ loopchain/blockchain/transactions/transaction.py
@@ -6,7 +6,7 @@ from .. import Hash32, Signature
 if TYPE_CHECKING:
     from .. import TransactionVersioner
 
-size_dict_name = "_size_dict"
+_size_attr_name_ = "_size_attr_"
 
 
 @dataclass(frozen=True)
@@ -29,16 +29,11 @@ class Transaction:
     def size(self, versioner: 'TransactionVersioner'):
         from .. import TransactionSerializer
 
-        if not hasattr(self, size_dict_name):
-            object.__setattr__(self, size_dict_name, dict())
-
-        size_dict = getattr(self, size_dict_name)
-        hash_generator_version = versioner.get_hash_generator_version(self.version)
-        if hash_generator_version not in size_dict:
+        if not hasattr(self, _size_attr_name_):
             ts = TransactionSerializer.new(self.version, versioner)
             tx_serialized = ts.to_full_data(self)
             tx_serialized = json.dumps(tx_serialized)
             tx_serialized = tx_serialized.encode('utf-8')
-            size_dict[hash_generator_version] = len(tx_serialized)
+            object.__setattr__(self, _size_attr_name_, len(tx_serialized))
 
-        return size_dict[hash_generator_version]
+        return getattr(self, _size_attr_name_)
