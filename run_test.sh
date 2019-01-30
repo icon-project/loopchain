a84a612552767a543dc6fcff508710e0c983b239
Jiyun Park <junepark@theloop.co.kr>
2019-01-30 18:27:37 +0900

    [LC-130] remove unusing script files

--- run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# This file will be deleted after changing the test method in CI.
-
-python3 -m unittest discover testcase/unittest/ -p "test_*.py" || exit -1
-#python3 -m unittest -q testcase.unittest.test_peer.TestPeer.test_query
