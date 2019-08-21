braveheart12/ICON-Chain

Remove the "ALLOW" word in name of properties that aren't the boolean type in `configure_default.py`.
- Align a code.
- remove an unnecessary property.

loopchain/blockchain/epoch.py
@@ -162,9 +162,9 @@ class Epoch:
             if tx is None:
                 break
 
-            if not util.is_in_time_boundary(tx.timestamp, conf.ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK):
-                util.logger.info(f"fail add tx to block by ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK"
-                                 f"({conf.ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK}) "
+            if not util.is_in_time_boundary(tx.timestamp, conf.TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK):
+                util.logger.info(f"fail add tx to block by TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK"
+                                 f"({conf.TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK}) "
                                  f"tx({tx.hash}), timestamp({tx.timestamp})")
                 continue
 

icon-project/loopchain

loopchain/blockchain/epoch.py
@@ -146,9 +146,9 @@ class Epoch:
             if tx is None:
                 break
 
-            if not utils.is_in_time_boundary(tx.timestamp, conf.ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK):
-                utils.logger.info(f"fail add tx to block by ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK"
-                                  f"({conf.ALLOW_TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK}) "
+            if not utils.is_in_time_boundary(tx.timestamp, conf.TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK):
+                utils.logger.info(f"fail add tx to block by TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK"
+                                  f"({conf.TIMESTAMP_BOUNDARY_SECOND_IN_BLOCK}) "
                                   f"tx({tx.hash}), timestamp({tx.timestamp})")
                 continue
 