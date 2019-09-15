5c77b4fadc113426fcc84e0bba5daec749e8695c
Seongjun Ji <metalg0su@gmail.com>
2019-09-15 18:16:15 +0900

    [LC-588] Add exception classes for configure

---
+++ testcase/integration/configure/exceptions.py
@@ -0,0 +1,14 @@
+class NotFoundChannelConfig(Exception):
+    pass
+
+
+class NotFoundBlockVersions(Exception):
+    pass
+
+
+class NotFoundPeerConfig(Exception):
+    pass
+
+
+class NotFoundChannelManageDataPath(Exception):
+    pass
