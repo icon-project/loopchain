d39bb39f58e8103e1f810ee18883df2da33f3ae2
Seongjun Ji <metalg0su@gmail.com>
2019-09-24 17:25:17 +0900

    [LC-609] Integrate original unit tests
    - Move tx_verifier_cache test and refactoring it

--- testcase/unittest/blockchain/transactions/test_transaction_.py
+++ testcase/unittest/blockchain/transactions/test_transaction_.py
@@ -4,35 +4,34 @@ from loopchain.blockchain.transactions import Transaction, TransactionVersioner
 from loopchain.blockchain.transactions import genesis, v2, v3
 
 
-@pytest.mark.parametrize("version", [genesis.version, v2.version, v3.version])
+@pytest.mark.parametrize("tx_version", [genesis.version, v2.version, v3.version])
 class TestTransactions:
-    def test_type_is_none(self, tx_factory, version):
-        tx: Transaction = tx_factory(version=version)
+    def test_type_is_none(self, tx_factory, tx_version):
+        tx: Transaction = tx_factory(tx_version=tx_version)
 
         assert not tx.type()
 
-    def test_size_attribute_has_set_correctly(self, tx_factory, version):
+    def test_size_attribute_has_set_correctly(self, tx_factory, tx_version):
         from loopchain.blockchain.transactions.transaction import _size_attr_name_
 
-        tx: Transaction = tx_factory(version=version)
+        tx: Transaction = tx_factory(tx_version=tx_version)
         assert not hasattr(tx, _size_attr_name_)
 
         tx.size(versioner=TransactionVersioner())
         assert getattr(tx, _size_attr_name_)
 
-    def test_is_signed(self, tx_factory, version):
-        tx: Transaction = tx_factory(version=version)
+    def test_is_signed(self, tx_factory, tx_version):
+        tx: Transaction = tx_factory(tx_version=tx_version)
 
-        if version == genesis.version:
-            # TODO: is this right test?
+        if tx_version == genesis.version:
             pass
         else:
             assert tx.is_signed()
 
-    def test_signer_address(self, tx_factory, version):
-        tx: Transaction = tx_factory(version=version)
+    def test_signer_address(self, tx_factory, tx_version):
+        tx: Transaction = tx_factory(tx_version=tx_version)
 
-        if version == genesis.version:
+        if tx_version == genesis.version:
             with pytest.raises(NotImplementedError):
                 assert tx.signer_address
         else:
--- testcase/unittest/blockchain/transactions/test_transaction_verifier_cache.py
+++ testcase/unittest/blockchain/transactions/test_transaction_verifier_cache.py
@@ -5,9 +5,8 @@ import pytest
 
 from loopchain.blockchain.exception import TransactionInvalidHashError
 from loopchain.blockchain.exception import TransactionInvalidSignatureError
-from loopchain.blockchain.transactions import TransactionBuilder
-from loopchain.blockchain.transactions import TransactionVerifier
-from loopchain.blockchain.transactions import TransactionVersioner
+from loopchain.blockchain.transactions import TransactionBuilder, TransactionVerifier, TransactionVersioner
+from loopchain.blockchain.transactions import genesis, v2, v3, v3_issue
 from loopchain.blockchain.types import ExternalAddress
 from loopchain.blockchain.types import Hash32
 from loopchain.blockchain.types import Signature
@@ -16,29 +15,16 @@ from loopchain.crypto.signature import Signer
 tx_versioner = TransactionVersioner()
 
 
+@pytest.mark.parametrize("tx_version", [
+    v2.version, v3.version
+])
 class TestTxCache:
     target_attrs = ["hash", "signature"]
 
-    @pytest.fixture
-    def tx(self):
-        """Transaction fixture"""
-        tb = TransactionBuilder.new(version="0x3", type_=None, versioner=tx_versioner)
-        tb.step_limit = 1000000
-        tb.value = 100000
-        tb.signer = Signer.from_prikey(os.urandom(32))
-        tb.to_address = ExternalAddress(os.urandom(20))
-        tb.nid = 3
-        tb.nonce = random.randint(0, 100000)
-        tb.data = "test"
-        tb.data_type = "message"
-
-        transaction = tb.build()
-
-        return transaction
-
     @pytest.mark.parametrize("target_attr", target_attrs)
-    def test_no_cache_attr_in_tx_if_not_verified(self, tx, target_attr):
+    def test_no_cache_attr_in_tx_if_not_verified(self, tx_version, tx_factory, target_attr):
         """Check that Transaction has no attribute until the verification func has been invoked"""
+        tx = tx_factory(tx_version=tx_version)
         if "cache" in dir(tx):
             raise AttributeError("Something wrong. Transaction has cached value when initialized.")
 
@@ -47,9 +33,10 @@ class TestTxCache:
 
     @pytest.mark.parametrize("raise_exception", [True, False])
     @pytest.mark.parametrize("target_attr", ["hash", "signature"])
-    def test_has_cache_attr_when_verified_successfully(self, tx, raise_exception, target_attr):
+    def test_has_cache_attr_when_verified_successfully(self, tx_version, tx_factory, raise_exception, target_attr):
         """Check that the verification result has been cached successfully"""
-        tv = TransactionVerifier.new(tx.version,  tx.type(), tx_versioner, raise_exceptions=raise_exception)
+        tx = tx_factory(tx_version=tx_version)
+        tv = TransactionVerifier.new(tx.version, tx.type(), tx_versioner, raise_exceptions=raise_exception)
 
         verify_func_name = f"verify_{target_attr}"
         verify_func = getattr(tv, verify_func_name)
@@ -62,8 +49,9 @@ class TestTxCache:
         (TransactionInvalidHashError, "hash", Hash32.new()),
         (TransactionInvalidSignatureError, "signature", Signature.new()),
     ])
-    def test_exception_cached_when_raised_exception_while_verification(self, tx, raise_exception, expected_exc, target_attr, fake_value, monkeypatch):
+    def test_exception_cached_when_raised_exception_while_verification(self, tx_version, tx_factory, raise_exception, expected_exc, target_attr, fake_value, monkeypatch):
         """Check that the exception successfully cached when raised any exception while verification step"""
+        tx = tx_factory(tx_version=tx_version)
         tv = TransactionVerifier.new(tx.version,  tx.type(), tx_versioner, raise_exceptions=raise_exception)
         verify_func = getattr(tv, f"verify_{target_attr}")
 
@@ -90,12 +78,13 @@ class TestTxCache:
         (TransactionInvalidHashError, "hash", Hash32.new()),
         (TransactionInvalidSignatureError, "signature", Signature.new()),
     ])
-    def test_verify_success_and_no_exc_with_fake_value_at_second(self, tx, raise_exception, expected_exc, target_attr, fake_value, monkeypatch):
+    def test_verify_success_and_no_exc_with_fake_value_at_second(self, tx_version, tx_factory, raise_exception, expected_exc, target_attr, fake_value, monkeypatch):
         """Check that the result is successfully cached and bypasses further verifications which could raise exceptions.
 
         Do not apply this usecase in code!
         This test aims the reliablity of cache logic, not for the usefulness of this case.
         """
+        tx = tx_factory(tx_version=tx_version)
         tv = TransactionVerifier.new(tx.version,  tx.type(), tx_versioner, raise_exceptions=raise_exception)
         verify_func = getattr(tv, f"verify_{target_attr}")
 
@@ -115,10 +104,11 @@ class TestTxCache:
             verify_func(tx)
             assert not tv.exceptions
 
-    @pytest.mark.parametrize("tag", ["first", "second"])
+    @pytest.mark.parametrize("tag", ["before_cache", "after_cache"])
     @pytest.mark.parametrize("target_attr", ["hash", "signature"])
-    def test_benchmark_verify(self, benchmark, tx, target_attr, tag):
+    def test_benchmark_verify(self, benchmark, tx_version, tx_factory, target_attr, tag):
         """Benchmark the elapsed time of verification func in various cases."""
+        tx = tx_factory(tx_version=tx_version)
         tv = TransactionVerifier.new(tx.version,  tx.type(), tx_versioner)
         verify_func = getattr(tv, f"verify_{target_attr}")
 
@@ -127,4 +117,3 @@ class TestTxCache:
         else:
             verify_func(tx)
             benchmark(verify_func, tx)
-
