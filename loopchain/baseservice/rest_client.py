de17c5ad6641d8b17f39a172ac94710687a03807
Daehee Kim <daehee.kim@theloop.co.kr>
2019-10-15 14:57:20 +0900

    [LC-679] Add `call_async` to `RestClient`

--- loopchain/baseservice/rest_client.py
+++ loopchain/baseservice/rest_client.py
@@ -15,13 +15,15 @@
 
 import asyncio
 import logging
+import time
 from collections import namedtuple
-from typing import List, Optional, NamedTuple
+from typing import List, Optional, NamedTuple, Sequence
 from urllib.parse import urlparse
 
 import requests
 from aiohttp import ClientSession
 from jsonrpcclient import HTTPClient, Request
+from jsonrpcclient.aiohttp_client import aiohttpClient
 from loopchain import utils, configure as conf
 
 
@@ -50,22 +52,17 @@ class RestClient:
     def target(self):
         return self._target
 
-    async def _fetch_status(self, session: ClientSession, request_uri):
-        endpoint_target = f"{urlparse(request_uri).scheme}://{urlparse(request_uri).netloc}"
-        start_time = session.loop.time()
-        async with session.get(url=request_uri,
-                               params={'channel': self._channel_name},
-                               timeout=conf.REST_TIMEOUT) as response:
-            response_dict = await response.json()
-            block_height = response_dict['block_height']
-            elapsed_time = session.loop.time() - start_time
-            return {
-                'target': endpoint_target,
-                'elapsed_time': elapsed_time,
-                'height': block_height
-            }
+    async def _fetch_status(self, endpoint: str):
+        start_time = time.time()
+        response = await self._call_async_rest(endpoint, RestMethods.Status, conf.REST_TIMEOUT)
 
-    async def _select_fastest_endpoint(self, endpoints) -> Optional[str]:
+        return {
+            'target': endpoint,
+            'elapsed_time': time.time() - start_time,
+            'height': response['block_height']
+        }
+
+    async def _select_fastest_endpoint(self, endpoints: Sequence[str]) -> Optional[str]:
         """select fastest endpoint with conditions below
         1. Maximum block height (higher priority)
         2. Minimum elapsed response time
@@ -73,12 +70,9 @@ class RestClient:
         :param endpoints: list of endpoints
         :return: the fastest endpoint target "{scheme}://{netloc}"
         """
-        path = RestMethods.Status.name
-        endpoints = [utils.normalize_request_url(endpoint, conf.ApiVersion.v1) + path for endpoint in endpoints]
-        async with ClientSession() as session:
-            results = await asyncio.gather(*[self._fetch_status(session, endpoint) for endpoint in endpoints],
-                                           return_exceptions=True)
-            results = [result for result in results if isinstance(result, dict)]  # to filter exceptions
+        results = await asyncio.gather(*[self._fetch_status(endpoint) for endpoint in endpoints],
+                                       return_exceptions=True)
+        results = [result for result in results if isinstance(result, dict)]  # to filter exceptions
 
         if not results:
             logging.warning(f"no alive node among endpoints({endpoints})")
@@ -87,25 +81,43 @@ class RestClient:
         # sort results by min elapsed_time with max block height
         sorted_result = sorted(results, key=lambda k: (-k['height'], k['elapsed_time']))
         min_latency_target = sorted_result[0]['target']
-        logging.info(f"minimum latency endpoint is: {min_latency_target}")
-        return min_latency_target
+        normalized_target = utils.normalize_request_url(min_latency_target)
+        normalized_target = f"{urlparse(normalized_target).scheme}://{urlparse(normalized_target).netloc}"
+        logging.info(f"minimum latency endpoint is: {normalized_target}")
+        return normalized_target
 
     def call(self, method: RestMethod, params: Optional[NamedTuple] = None, timeout=None) -> dict:
         timeout = timeout or conf.REST_ADDITIONAL_TIMEOUT
 
         try:
             if method.version == conf.ApiVersion.v1:
-                response = self._call_rest(method, timeout)
+                response = self._call_rest(self.target, method, timeout)
             else:
-                response = self._call_jsonrpc(method, params, timeout)
-            utils.logger.spam(f"REST call complete method_name({method.name})")
-            return response
+                response = self._call_jsonrpc(self.target, method, params, timeout)
         except Exception as e:
             logging.warning(f"REST call fail method_name({method.name}), caused by : {type(e)}, {e}")
             raise e
+        else:
+            utils.logger.spam(f"REST call complete method_name({method.name})")
+            return response
 
-    def _call_rest(self, method: RestMethod, timeout):
-        url = utils.normalize_request_url(self._target, method.version, self._channel_name)
+    async def call_async(self, method: RestMethod, params: Optional[NamedTuple] = None, timeout=None) -> dict:
+        timeout = timeout or conf.REST_ADDITIONAL_TIMEOUT
+
+        try:
+            if method.version == conf.ApiVersion.v1:
+                response = await self._call_async_rest(self.target, method, timeout)
+            else:
+                response = await self._call_async_jsonrpc(self.target, method, params, timeout)
+        except Exception as e:
+            logging.warning(f"REST call async fail method_name({method.name}), caused by : {type(e)}, {e}")
+            raise e
+        else:
+            utils.logger.spam(f"REST call async complete method_name({method.name})")
+            return response
+
+    def _call_rest(self, target: str, method: RestMethod, timeout):
+        url = utils.normalize_request_url(target, method.version, self._channel_name)
         url += method.name
         response = requests.get(url=url,
                                 params={'channel': self._channel_name},
@@ -114,8 +126,8 @@ class RestClient:
             raise ConnectionError
         return response.json()
 
-    def _call_jsonrpc(self, method: RestMethod, params: Optional[NamedTuple], timeout):
-        url = utils.normalize_request_url(self._target, method.version, self._channel_name)
+    def _call_jsonrpc(self, target: str, method: RestMethod, params: Optional[NamedTuple], timeout):
+        url = utils.normalize_request_url(target, method.version, self._channel_name)
         http_client = HTTPClient(url)
 
         # 'vars(namedtuple)' does not working in Python 3.7.4
@@ -125,3 +137,26 @@ class RestClient:
             return http_client.send(request, timeout=timeout)
         except Exception as e:
             raise ConnectionError(e)
+
+    async def _call_async_rest(self, target: str, method: RestMethod, timeout):
+        url = utils.normalize_request_url(target, method.version, self._channel_name)
+        url += method.name
+
+        async with ClientSession() as session:
+            async with session.get(url=url,
+                                   params={'channel': self._channel_name},
+                                   timeout=timeout) as response:
+                return await response.json()
+
+    async def _call_async_jsonrpc(self, target: str, method: RestMethod, params: Optional[NamedTuple], timeout):
+        url = utils.normalize_request_url(target, method.version, self._channel_name)
+        async with ClientSession() as session:
+            http_client = aiohttpClient(session, url)
+
+            # 'vars(namedtuple)' does not working in Python 3.7.4
+            # noinspection PyProtectedMember
+            request = Request(method.name, params._asdict()) if params else Request(method.name)
+            try:
+                return await http_client.send(request, timeout=timeout)
+            except Exception as e:
+                raise ConnectionError(e)
