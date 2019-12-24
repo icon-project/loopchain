0660322ef7b9e5b47644117c31f925d3ac7f9eba
Heizel Kim <hrkim0624@gmail.com>
2019-12-24 18:03:47 +0900

    [LC-748] Edit block data to implement `v0.4` by inheritance of `v0.3`.
    - Optimized imports in some files.

--- loopchain/blockchain/blocks/v0_4/__init__.py
+++ loopchain/blockchain/blocks/v0_4/__init__.py
@@ -1,4 +1,4 @@
-from .block import BlockHeader, BlockBody, receipts_hash_generator
+from .block import BlockHeader, BlockBody
 from .block_prover import BlockProver
 from .block_builder import BlockBuilder
 from .block_serializer import BlockSerializer
