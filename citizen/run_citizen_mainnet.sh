2de87e227710eee67985d9239ac47743f251d3ce
Seungmin.Ryu <yakkle@gmail.com>
2020-01-20 11:17:26 +0900

    [LC-864] add block version 0.5 height to configuration
     - mainnet : 14473622
     - testnet : 524360
     - set mainPrepCount by 7 for testnet

--- citizen/run_citizen_mainnet.sh
+++ citizen/run_citizen_mainnet.sh
@@ -6,7 +6,7 @@ echo "Run citizen with port 7100..."
 loopchain -o ${CONF_DIR}/citizen_mainnet.json &
 
 echo "Run iconservice for citizen 7100 start!"
-iconservice start -c ${CONF_DIR}/iconservice_mainnet.json &
+iconservice start -c ${CONF_DIR}/iconservice_mainnet.json
 
 echo "Run iconrpcserver for citizen 7100 start!"
-iconrpcserver start -c ${CONF_DIR}/iconrpcserver_mainnet.json &
+iconrpcserver start -c ${CONF_DIR}/iconrpcserver_mainnet.json
--- citizen/run_citizen_testnet.sh
+++ citizen/run_citizen_testnet.sh
@@ -6,7 +6,7 @@ echo "Run citizen with port 7100..."
 loopchain -o ${CONF_DIR}/citizen_testnet.json &
 
 echo "Run iconservice for citizen 7100 start!"
-iconservice start -c ${CONF_DIR}/iconservice_testnet.json &
+iconservice start -c ${CONF_DIR}/iconservice_testnet.json
 
 echo "Run iconrpcserver for citizen 7100 start!"
-iconrpcserver start -c ${CONF_DIR}/iconrpcserver_testnet.json &
+iconrpcserver start -c ${CONF_DIR}/iconrpcserver_testnet.json
