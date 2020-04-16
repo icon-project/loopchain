81e12ed9d69b658b4a02efedc2d100e9d6e83c5a
Seongjun Ji <metalg0su@gmail.com>
2020-04-16 11:42:50 +0900

    [LC-943] Do dummy jobs for future works
    - Add dummy BlockVerifier
    - Add temporary methods in KeyValueStorePlyvel

---
+++ loopchain/blockchain/blocks/v1_0/block_verifier.py
@@ -0,0 +1,9 @@
+from lft.consensus.messages.data import DataVerifier
+
+
+class BlockVerifier(DataVerifier):
+    def __init__(self):
+        pass
+
+    async def verify(self, data: 'Data'):
+        pass
