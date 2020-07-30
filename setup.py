3655aa7675f89999394e09e206cb2d358f6c2bed
Seungmin.Ryu <yakkle@gmail.com>
2020-07-30 12:50:50 +0900

    update requires compatible on Makefile
     - compatible version with iconservice 1.7.0, iconrpcserver 1.5.0
     - update iconsdk compatible version with 1.3.0

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup_options = {
     'setup_requires': setup_requires,
     'install_requires': install_requires,
     'extras_require': {
-        'tests': ['iconsdk==1.1.0', 'pytest>=4.6.3', 'pytest-xprocess>=0.12.1', "pytest-benchmark", "pytest-mock",
+        'tests': ['iconsdk~=1.3.0', 'pytest>=4.6.3', 'pytest-xprocess>=0.12.1', "pytest-benchmark", "pytest-mock",
                   "pytest-asyncio", "freezegun"],
     },
     'entry_points': {
